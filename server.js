dc39bb1ddf1a15cbc2618b382fe4c849479df6ef
Pete-Scale <petescale@gmail.com>
2021-02-02 10:03:25 -0600

    added html routes

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('Note Taker server is working!');
+});
+
+/**
+ * HTML ROUTES
+ */
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
