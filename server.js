166b57f731a78f816876a490a1c472ebf0c9b680
Pete-Scale <petescale@gmail.com>
2021-02-02 10:24:37 -0600

    require notesArray and api get notes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+let notesArray = require('./db/db.json');
+
 app.get('/', (req, res) => {
     res.send('Note Taker server is working!');
 });
@@ -17,6 +19,16 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+/**
+ * API ROUTES
+ */
+// show all saved notes
+app.get("/api/notes", (req, res) => {
+    res.json(notesArray);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
